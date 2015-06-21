752eae6d758827c542b53e1409a375b9f705cda3
lamquoctuan <lamquoctuan@gmail.com>
2015-06-21 11:55:30 -0500

    kick off

---
+++ wp-content/themes/vc4g/footer.php
@@ -0,0 +1,12 @@
+	</div><!-- .site-content -->
+
+	<footer id="colophon" class="site-footer">
+        FOOTER
+	</footer><!-- .site-footer -->
+
+</div><!-- .site -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
