78b61b6955e88379c63844956487b5b08057440f
lamquoctuan <lamquoctuan@gmail.com>
2015-07-03 14:19:14 -0500

    add page template

---
+++ assets/themes/vc4g/template/in-store-service.php
@@ -0,0 +1,137 @@
+<section class="bg-banner-how">
+    <div class="container">
+        <h2>IN-STORE Service</h2>
+
+        <p>As you arrive at 3515 Kingsway, you will be greeted in a friendly manner by one of our buyers.<br/>There is
+            typically little or no wait. Here is the process on how to get cash for your gold!</p>
+    </div>
+</section>
+
+<!-- Team Section -->
+<section id="" class="bg-light-white about-page howtosell">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-sm-9">
+                <div class="akordeon">
+                    <div class="akordeon-item">
+                        <div class="akordeon-item-head">
+                            <div class="akordeon-item-head-container">
+                                <div class="akordeon-heading">
+                                    SORTING BY KARAT
+                                </div>
+                            </div>
+                        </div>
+                        <div class="akordeon-item-body">
+                            <div class="akordeon-item-content">
+                                <p>The first thing we do is sort out items that are not gold or silver. To do this we
+                                    use a Rare Earth Magnet. The Rare Earth Magnet is an incredibly powerful permanent
+                                    magnet made from alloys of rare earth elements. Gold, Silver, and Platinum are not
+                                    magnetic. This catches some plated items immediately. Unfortunately we are unable to
+                                    purchase plated pieces as the cost of refining them exceeds the value of the metal
+                                    we can extract from the item. However, there are many non-valuable metals that are
+                                    also non-magnetic.</p>
+
+                                <p>The second step in sorting involves checking for stamps. Many stamps are visible to
+                                    the naked eye but some are stamped on the tiny posts of earrings or are too worn
+                                    down to make out. In this case we use a jeweller's loop to closely examine each
+                                    piece. Stamps tell you what the item is sold as and is a fairly reliable way of
+                                    identifying the gold purity. However, it is not rare to find an item stamped higher
+                                    than its actual purity.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="akordeon-item">
+                        <div class="akordeon-item-head">
+                            <div class="akordeon-item-head-container">
+                                <div class="akordeon-heading">
+                                    WEIGHING
+                                </div>
+                            </div>
+                        </div>
+                        <div class="akordeon-item-body">
+                            <div class="akordeon-item-content">
+                                Contents Here
+                            </div>
+                        </div>
+                    </div>
+                    <div class="akordeon-item">
+                        <div class="akordeon-item-head">
+                            <div class="akordeon-item-head-container">
+                                <div class="akordeon-heading">
+                                    QUOTING
+                                </div>
+                            </div>
+                        </div>
+                        <div class="akordeon-item-body">
+                            <div class="akordeon-item-content">
+                                Contents Here
+                            </div>
+                        </div>
+                    </div>
+                    <div class="akordeon-item last">
+                        <div class="akordeon-item-head">
+                            <div class="akordeon-item-head-container">
+                                <div class="akordeon-heading">
+                                    STONE REMOVAL AND PAYMENT
+                                </div>
+                            </div>
+                        </div>
+                        <div class="akordeon-item-body">
+                            <div class="akordeon-item-content">
+                                Contents Here
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3 sidebar-whywepay">
+                <a class="btn request">Mail-in Request Form</a>
+
+                <h3>Recent Pay</h3>
+
+                <div id="myCarousel" class="carousel slide recentpay">
+                    <!-- Wrapper for Slides -->
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <!-- Set the first background image using inline CSS below. -->
+                            <img src="/assets/images/recent-pay/img1.jpg"/>
+
+                            <div class="carousel-caption pt1">
+                                <h2>$2079.55</h2>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <!-- Set the second background image using inline CSS below. -->
+                            <img src="/assets/images/recent-pay/img2.jpg"/>
+
+                            <div class="carousel-caption pt1">
+                                <h2>$2079.55</h2>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <!-- Set the third background image using inline CSS below. -->
+                            <img src="/assets/images/recent-pay/img3.jpg"/>
+
+                            <div class="carousel-caption pt1">
+                                <h2>$2079.55</h2>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Controls -->
+                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                        <span class="icon-prev"></span>
+                    </a>
+                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                        <span class="icon-next"></span>
+                    </a>
+
+                </div>
+
+
+            </div>
+        </div>
+
+    </div>
+</section>
