1c87e300b702254baaef8464f56a7b014b487c71
lamquoctuan <lamquoctuan@gmail.com>
2015-07-08 12:17:59 -0500

    add blog template

--- assets/themes/vc4g/footer.php
+++ assets/themes/vc4g/footer.php
@@ -1,5 +1,7 @@
 <?php
-get_template_part( 'template/footer', 'onedaydeal' );
+if (! (is_single() || is_page('buy-diamond'))) {
+    get_template_part( 'template/footer', 'onedaydeal' );
+}
 get_template_part( 'template/footer', 'contact' );
 get_template_part( 'template/footer' );
 ?>
---
+++ assets/themes/vc4g/template/blog.php
@@ -0,0 +1,70 @@
+<section class="webuy diamond blog">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-8">
+                <div class="body-blog">
+                    <h2>Who We Are The #1 Vancouver Gold Buyer</h2>
+                    <p class="small">By ADMIN  In Beauty  Posted March 9, 2015</p>
+                    <p><img src="img/img-blog.jpg" alt="intro-img" /></p>
+                    <p>What a difference, I was quoted $10 a gram for 10kt gold at this mall kiosk by someone who did not seem like they knew what they were doing. Vancouver cash for gold paid me over $20 per gram; I will only be dealing with them in the future, What a difference, I was quoted $10 a gram for 10kt gold at this mall kiosk by someone who did not seem like they knew what they were doing. Vancouver cash for gold paid me over $20 per gram; </p>
+                    <p>I will only be dealing with them in the futureWhat a difference, I was quoted $10 a gram for 10kt gold at this mall kiosk by someone who did not seem like they knew what they were doing. Vancouver cash for gold paid me over $20 per gram; I will only be dealing with them in the future.</p>
+                    <ul>
+                        <li>Ta dkf asflo fdskfjp fdfkd</li>
+                        <li>Ta dkf asflo fdskfjp fdfkd</li>
+                        <li>Ta dkf asflo fdskfjp fdfkd</li>
+                        <li>Ta dkf asflo fdskfjp fdfkd</li>
+                    </ul>
+                </div>
+                <div class="pagination-share mt40">
+                    <div class="pagi">
+                        <a href="#" class="mr20"><i class="fa fa-angle-left"></i> Back</a><a href="#">Next <i class="fa fa-angle-right"></i></a>
+                    </div>
+                    <div class="share">
+                        <ul class="list-inline social-buttons">
+                            <li>Share</li>
+                            <li class="text-center"><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li class="text-center"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="pricing mt40">
+                    <h4>Download Pricing</h4>
+                    <form accept-charset="UTF-8" action=""  method="post" >
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Your Name" id="name" required="" data-validation-required-message="Please enter your name." aria-invalid="false">
+                            <p class="help-block text-danger"></p>
+                        </div>
+
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Email Address" id="email" required="" data-validation-required-message="Please enter your mail." aria-invalid="false">
+                            <p class="help-block text-danger"></p>
+                        </div>
+                    </form>
+                    <a href="" class="btn download"><span>DOWNLOAD</span></a>
+                    <p class="text-center small">Enter your name and email below and click, "DOWNLOAD!"</p>
+                </div>
+                <div class="recent-post mt30">
+                    <h4>Recent Posts</h4>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Seamlessly enable multimedia based technologies</a></li>
+                        <li><a href="#">Distinctively provide access to backend</a></li>
+                        <li><a href="#">Intrinsicly plagiarize interactive</a></li>
+                        <li><a href="#">Uniquely develop quality catalysts</a></li>
+                    </ul>
+                </div>
+                <div class="whatwepay mt30">
+                    <div class="text-ads">
+                        <div class="text-cnt">
+                            <img src="img/whatwepay.png"  alt=""/>
+                        </div>
+
+                    </div>
+                    <img src="img/img-ads.jpg" class="img-responsive" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
