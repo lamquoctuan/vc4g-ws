549cdb4fdf89eff33c3bb084f7f4934414e365fa
lumpynroo <lamquoctuan@hotmail.com>
2015-08-11 16:00:34 +0000

    add mail-in-service old content

--- assets/themes/vc4g/template/mail-in-service.php
+++ assets/themes/vc4g/template/mail-in-service.php
@@ -3,7 +3,8 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-7">
-                <?php get_template_part('template/mail-in-service', 'steps');?>
+                <?php // get_template_part('template/mail-in-service', 'steps');?>
+                <?php get_template_part('template/mail-in-service', 'steps-old');?>
             </div>
             <div class="col-sm-5">
                 <?php get_template_part('template/mail-in-service', 'form');?>
