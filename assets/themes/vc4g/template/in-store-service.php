ca0397ca49342d87512917e2139e4c96c634a857
lumpynroo <lamquoctuan@hotmail.com>
2015-08-23 09:54:56 +0000

    update contents the templage in-store-service

--- assets/themes/vc4g/template/in-store-service.php
+++ assets/themes/vc4g/template/in-store-service.php
@@ -51,7 +51,8 @@
                         </div>
                         <div class="akordeon-item-body">
                             <div class="akordeon-item-content">
-                                Contents Here
+                                <p>Once all the items have been sorted and verified by karat, we begin weighing the items on a scale. The scale is calibrated twice daily and you can ask to have it recalibrated if you like. Our scale will show you the true weight of your items in grams. Typically we weigh all items identified at a particular karat together but can also weigh individual items for you.</p>
+                                <p>If items have stones or inserts we will subtract the estimated weight of those stones. Gems the size of 1 carat diamonds will weigh approximately 0.2 grams. We assess the rough size and number of stones in the items and subtract this weight from our estimate. For example, if a piece has 4 small stones of the 1/10th carat size we will subtract 4 times 1/10th of 0.2 grams (ie. 0.08 grams).</p>
                             </div>
                         </div>
                     </div>
@@ -65,7 +66,8 @@
                         </div>
                         <div class="akordeon-item-body">
                             <div class="akordeon-item-content">
-                                Contents Here
+                                <p>Our prices are based on the current market price of gold and silver. Please reference What We Pay for the complete price table. Your payout is based on the purity (karat) and the weight of your items. After our assessment, you are not obligated to sell. We provide our assessments free of charge.</p>
+                                <p>Some large items may not be solid gold or silver. These items are tricky as they can be thickly plated, filled with weights or something of the like. A final test before payment may be required by the buyer to confirm the item is not fraudulent. This can involve cutting the piece and damaging the piece beyond repair. We will ask you before these types of tests. Essentially we have agreed to pay the quoted price if the item is not fraudulent. If you do not wish to have the test performed on your item, we will be happy to return it to you, but will not be able to purchase the item.</p>
                             </div>
                         </div>
                     </div>
@@ -79,7 +81,7 @@
                         </div>
                         <div class="akordeon-item-body">
                             <div class="akordeon-item-content">
-                                Contents Here
+                                <p>Once you are satisfied with our payout, the buyer will remove any easily removable stones and inserts and will do their best to remove any that you specifically request. On the open market small used stones have little to no value but you may be able to use them in the design of future jewelry. Once stones and inserts have been removed, the items are re-weighed to confirm the quote and the offer is adjusted accordingly. You will be paid on the spot without any hidden fees or charges.</p>
                             </div>
                         </div>
                     </div>
