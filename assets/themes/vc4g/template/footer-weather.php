657c56c4a230b570051206b22260dac9064ee533
lumpynroo <lamquoctuan@hotmail.com>
2015-08-23 10:07:40 +0000

    prevent notice if weather condition cannot load, update home services link

--- assets/themes/vc4g/template/footer-weather.php
+++ assets/themes/vc4g/template/footer-weather.php
@@ -3,9 +3,11 @@ include_once CUR_THEME_DIR . '/inc/Connectors/Yahoo.php';
 use \LNR\Connectors\Yahoo;
 $connector = new Yahoo();
 $condition = $connector->getCondition();
+if (! empty($condition)) {
 echo setWeatherIcon($condition->code);
 ?>
 <p><?php echo $condition->text;?></p>
 <!--<i class="fa icon-snow"></i>-->
 <p class="temperature"><?php echo $condition->temp;?><sup>o</sup></p>
 <p><?php echo setWarmMessageBy($condition->code);?></p>
+<?php } ?>
--- assets/themes/vc4g/template/section-services.php
+++ assets/themes/vc4g/template/section-services.php
@@ -14,7 +14,7 @@
                 </span>
             <h3 class="service-heading">A Trustable Buyer</h3>
             <p class="text-muted">We still offer you free and accurate estimates for the value of your jewelry even if you don’t sell to us.</p>
-            <button type="submit" class="btn body-content mt20" onclick="window.location.href='how-to-sell/';">Estimate The Payout</button>
+            <button type="submit" class="btn body-content mt20" onclick="window.location.href='what-we-buy/';">Estimate The Payout</button>
         </div>
         <div class="col-md-4">
                 <span class="fa-stack fa-4x">
