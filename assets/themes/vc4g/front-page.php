f793b0bbf022bace36b3cb902d10e2f67488bd12
lumpynroo <lamquoctuan@hotmail.com>
2015-09-03 04:20:57 +0000

    reorder homepate sections

--- assets/themes/vc4g/front-page.php
+++ assets/themes/vc4g/front-page.php
@@ -7,6 +7,9 @@ get_header();
     // Start the loop.
     while ( have_posts() ) : the_post();
         global $post;
+        get_template_part( 'template/home', 'whatwebuy' );
+        get_template_part( 'template/home', 'buydiamond' );
+        get_template_part( 'template/home', 'howtosell' );
         $content = $post->post_content;
         if (empty($content)) {
             get_template_part( 'template/home', 'services' );
@@ -14,9 +17,6 @@ get_header();
         else {
             echo $content;
         }
-        get_template_part( 'template/home', 'whatwebuy' );
-        get_template_part( 'template/home', 'buydiamond' );
-        get_template_part( 'template/home', 'howtosell' );
         get_template_part( 'template/home', 'blogs' );
         get_template_part( 'template/home', 'about' );
         // End the loop.
