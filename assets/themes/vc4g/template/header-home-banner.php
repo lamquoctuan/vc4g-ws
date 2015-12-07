9870a347acfe456eb700c5ed80c6c63d7e8aa85c
lumpynroo <lamquoctuan@hotmail.com>
2015-12-07 13:21:13 +0000

    update home banner

--- assets/themes/vc4g/template/header-home-banner.php
+++ assets/themes/vc4g/template/header-home-banner.php
@@ -1,20 +1,22 @@
 <!-- Header -->
 <header>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 text-left pt20">
-                <img src="/assets/images/logo.png" />
+    <div class="bg-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-left pt20">
+                    <img src="/assets/images/logo.png" />
+                </div>
+                <div class="col-md-6 text-right pt20">
+        			<a class="sub-phone" href="tel:16045582026"><b class="icon-phone"></b>604-558-2026</a>
+        			<span class="address-banner">3515 Kingsway, Vancouver, BC V5R 5L8</span>
+        		</div>
+        		<img src="/assets/images/gold.png" alt="goldvcg" />
             </div>
-            <div class="col-md-6 text-right pt20">
-    			<a class="sub-phone" href="tel:16045582026"><b class="icon-phone"></b>604-558-2026</a>
-    			<span class="address-banner">3515 Kingsway, Vancouver, BC V5R 5L8</span>
-    		</div>
         </div>
-        <div class="intro-text">
-            <div class="intro-lead-in">Instant Cash for Gold, Silver</div>
-            <div class="intro-heading">especially Diamond<acronym title=""><img src="/assets/images/logo-gia-small.jpg" alt=""></acronym></div>
-            <a href="#services" class="page-scroll btn btn-xl">How it works</a>
-            <p class="phone-banner">or call <strong><a href="tel:16045582026">604-558-2026</a></strong></p>
+    </div>    
+    <div class="ad-banner">
+        <div class="container">
+            Earn <strong>$50</strong> Extra Cash by <strong>Selling</strong> your gold or engagement rings
         </div>
     </div>
 </header>
