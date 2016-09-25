68d1299471d3e22a7630073a2beb4b290dfc8866
Tuan Lam <lamquoctuan@gmail.com>
2016-09-24 23:33:42 -0500

    init vc4g-m templates

---
+++ assets/themes/vc4g-m/front-page.php
@@ -0,0 +1,21 @@
+<?php
+get_header();
+?>
+
+<?php
+// Start the loop.
+while ( have_posts() ) : the_post();
+    global $post;
+    get_template_part( 'template/home', 'banner' );
+    get_template_part( 'template/home', 'gold-forecast' );
+    get_template_part( 'template/home', 'buy-diamond' );
+    get_template_part( 'template/home', 'about' );
+    get_template_part( 'template/home', 'gold-calculator' );
+    get_template_part( 'template/home', 'how-to-sell' );
+    get_template_part( 'template/home', 'gold-pricing' );
+    get_template_part( 'template/home', 'contact' );
+    // End the loop.
+endwhile;
+?>
+
+<?php get_footer(); ?>
