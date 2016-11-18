8b31c666664ef9d51a95e11db88a018f111c16a3
lumpynroo <lamquoctuan@hotmail.com>
2016-11-18 04:33:42 +0000

    get home banner back to default

---
+++ assets/themes/vc4g/template/header-home-banner-remembranch-day.php
@@ -0,0 +1,35 @@
+<!-- Header -->
+<header>
+    <div class="container mb20">
+        <div class="row">
+            <div class="col-md-6 text-left pt20">
+                <img src="/assets/images/logo.png" />
+            </div>
+            <div class="col-md-6 text-right pt20">
+    			<a class="sub-phone" href="tel:16045582026"><b class="icon-phone"></b>604-558-2026</a>
+    			<span class="address-banner">3515 Kingsway, Vancouver, BC V5R 5L8</span>
+    		</div>
+        </div>
+    </div>
+    
+    <div class="row row-no-padding" style="background:#e31d27;">
+        <div class="col-lg-5">
+            <div class="post post-variant-1">
+                <div class="post-inner">
+                    <img src="/assets/images/banners/img1-days.jpg" alt="" class="img-responsive post-image">
+                    <div class="post-caption">
+                        <div class="intro-text">
+                            <div class="intro-lead-in">Payment in <span>Cash Gold, Silver</span></div>
+                            <div class="intro-heading">We Buy Diamond <span>At Any Size</span></div>
+                            <a href="#services" class="page-scroll btn btn-xl">How it works</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-7 dpl-none days">
+            <img src="/assets/images/days.png" alt="" class="img-responsive post-image">
+        </div>
+    </div>
+</header>
+
