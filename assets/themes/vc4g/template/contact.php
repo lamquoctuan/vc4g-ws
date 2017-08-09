e4aa587585a09f98cffad1a834b77addf0ffef72
lumpynroo <lamquoctuan@hotmail.com>
2017-08-09 20:04:46 +0000

    add location surrey in contact page

--- assets/themes/vc4g/template/contact.php
+++ assets/themes/vc4g/template/contact.php
@@ -6,8 +6,11 @@
                     <div class="col-md-3 text-center">
                         <i class="fa icon-place-lg"></i>
                     </div>
-                    <div class="col-md-9">
-                        <p>3515 Kingsway,<br/>Vancouver, BC V5R 5L8<br/ >The Canada</p>
+                    <div class="col-md-4">
+                        <p>3515 Kingsway,<br/>Vancouver,<br/ >BC V5R 5L8<br/ >The Canada</p>
+                    </div>
+                    <div class="col-md-5">
+                        <p>10210 152 St #213,<br/>Surrey,<br/ >BC V3R 6N7</p>
                     </div>
                 </div>
             </div>
@@ -16,9 +19,13 @@
                     <div class="col-md-3 text-center">
                         <i class="fa icon-phone-lg"></i>
                     </div>
-                    <div class="col-md-9">
+                    <div class="col-md-5">
                         <p>Phone<br/><a href="16044494798">(604) 449-4798</a></p>
                         <p class="icon-phone-call"><a href="16044494798">(778) 882-8908</a></p>
+                    </div>
+                    <div class="col-md-4">
+                        <p>Cell<br/><a href="16049990606">(604) 999-0606</a></p>
+                        <p><a href="17788692112">(778) 869 2112</a></p>
                     </div>
                 </div>
             </div>
