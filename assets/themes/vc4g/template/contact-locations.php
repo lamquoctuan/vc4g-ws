d63ddfa4962b9dba61d99ca19d3411b7c8b3b0ee
lumpynroo <lamquoctuan@hotmail.com>
2017-08-19 05:13:44 +0000

    locations page update
    ;

---
+++ assets/themes/vc4g/template/contact-locations.php
@@ -0,0 +1,88 @@
+<section class="place-ct">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="col-md-3 text-center"></div>
+                    <div class="col-md-9">
+                        <h2 style="color: #d19f42;">Vancouver</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="row">
+                        <div class="col-md-3 text-center">
+                            <!--<i class="fa icon-place-lg"></i>-->
+                        </div>
+                        <div class="col-md-9">
+                            <p>3515 Kingsway,<br/>Vancouver, BC V5R 5L8<br/ >The Canada</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="row">
+                        <div class="col-md-9">
+                            <p>Phone<br/><a href="16045582026">(604) 558-2026</a></p>
+                            <p><a href="16044494798">(778) 882-8908</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="row">
+                        <!--<div class="col-md-3 text-center">-->
+                        <!--    <i class="fa icon-clock-lg"></i>-->
+                        <!--</div>-->
+                        <div class="col-md-9">
+                            <p>Mon - Fri: 10:00 am – 6:00 pm</p>
+                            <p>Saturday: 10:00 am – 5:00 pm</p>
+                            <p class="icon-email-small">info@vancouvercashforgold.com</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="col-md-3 text-center"></div>
+                    <div class="col-md-9">
+                        <h2 style="color: #d19f42;">Surrey</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="row">
+                        <div class="col-md-3 text-center">
+                            <!--<i class="fa icon-place-lg"></i>-->
+                        </div>
+                        <div class="col-md-9">
+                            <p>10210 152 St #213,<br/>Surrey, BC V3R 6N7<br/ >The Canada</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="row">
+                        <div class="col-md-9">
+                        <p>Phone<br/><a href="16049990606">(604) 999-0606</a></p>
+                        <p><a href="17788692112">(778) 869-2112</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="row">
+                        <!--<div class="col-md-3 text-center">-->
+                        <!--    <i class="fa icon-clock-lg"></i>-->
+                        <!--</div>-->
+                        <div class="col-md-9">
+                            <p>Mon - Sat: 10:00 am – 6:00 pm</p>
+                            <p></p>
+                            <p class="icon-email-small">surreycashforgold@gmail.com</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
